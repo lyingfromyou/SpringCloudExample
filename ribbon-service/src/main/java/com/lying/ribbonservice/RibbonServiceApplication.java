4fada5e286f81e5b7abd10c4e326ce082e5388a1
lying <15717302379@163.com>
2018-03-07 14:16:51 +0800

    ribbon

--- ribbon-service/src/main/java/com/lying/ribbonservice/RibbonServiceApplication.java
+++ ribbon-service/src/main/java/com/lying/ribbonservice/RibbonServiceApplication.java
@@ -2,9 +2,23 @@ package com.lying.ribbonservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableDiscoveryClient
 public class RibbonServiceApplication {
+
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RibbonServiceApplication.class, args);
--- ribbon-service/src/main/java/com/lying/ribbonservice/service/UserService.java
+++ ribbon-service/src/main/java/com/lying/ribbonservice/service/UserService.java
@@ -1,5 +1,6 @@
 package com.lying.ribbonservice.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +10,12 @@ public class UserService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "getUserInfoError")
     public String getUserInfo(String name) {
         return restTemplate.getForObject("http://eureka-client/users/" + name, String.class);
+    }
+
+    public String getUserInfoError(String name){
+        return "ribbon get user error!";
     }
 }
