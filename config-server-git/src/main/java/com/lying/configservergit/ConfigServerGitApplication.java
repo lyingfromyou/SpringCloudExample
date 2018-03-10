c899323ee332262fd49f6603c8ef15e194b97162
lying <15717302379@163.com>
2018-03-10 14:50:15 +0800

    config server

---
+++ config-server-git/src/main/java/com/lying/configservergit/ConfigServerGitApplication.java
@@ -0,0 +1,15 @@
+package com.lying.configservergit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerGitApplication.class, args);
+	}
+}
